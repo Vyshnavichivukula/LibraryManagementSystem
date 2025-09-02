ad6b444657c3ac141b672b40957911a5f441b876
Vyshnavichivukula <124250847+Vyshnavichivukula@users.noreply.github.com>
2025-09-02 13:07:59 +0530

    Create Library.java

---
+++ Library.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    private List<Book> books;
+
+    public Library() {
+        books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+        System.out.println(book.getTitle() + " added to library.");
+    }
+
+    public void showBooks() {
+        if (books.isEmpty()) {
+            System.out.println("No books in library.");
+            return;
+        }
+        for (Book book : books) {
+            System.out.println(book);
+        }
+    }
+
+    public Book findBookById(int id) {
+        for (Book book : books) {
+            if (book.getId() == id) {
+                return book;
+            }
+        }
+        return null;
+    }
+}
